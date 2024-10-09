292b10689d1800ad99b1b1ba7e9079753708e70f
Tom Schnetzer <160368069+tschnetz@users.noreply.github.com>
2024-10-08 23:19:43 -0400

    Version 6 - Standings added

--- google-deploy.sh
+++ google-deploy.sh
@@ -3,8 +3,8 @@
   #	•	Go to the project selector and click New Project.
   #	•	Give your project a name and note down the project ID.
 # Create a Dockerfile in the root of your project directory
-docker build -t streamlit-app .
+docker build -t cfb-app .
 # gcloud auth login
 gcloud config set project cfb-data-2024
-gcloud builds submit --tag gcr.io/cfb-data-2024/streamlit-app
-gcloud run deploy streamlit-app --image gcr.io/cfb-data-2024/streamlit-app --platform managed --region us-east1 --allow-unauthenticated
+gcloud builds submit --tag gcr.io/cfb-data-2024/cfb-app
+gcloud run deploy cfb-app --image gcr.io/cfb-data-2024/cfb-app --platform managed --region us-east1 --allow-unauthenticated
